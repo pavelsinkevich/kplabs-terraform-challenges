7d5e4731ddfb2ecf0dd979a2e441fc214d9e4c93
pavel_sinkevich1 <pavel.sinkevich@gmail.com>
2023-10-21 14:25:11 +0300

    used auto formatting for tf file

--- challenge-2/tf-challenge-2.tf
+++ challenge-2/tf-challenge-2.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
 
     }
   }
@@ -14,44 +14,44 @@ provider "aws" {
 
 resource "aws_security_group" "security_group_payment_app" {
   name        = "payment_app"
-    description = "Application Security Group"
-  depends_on = [aws_eip.example]
+  description = "Application Security Group"
+  depends_on  = [aws_eip.example]
 
-# Below ingress allows HTTPS  from DEV VPC
+  # Below ingress allows HTTPS  from DEV VPC
   ingress {
-       from_port        = var.HTTPS_DEV_port
-     to_port          = var.HTTPS_DEV_port
-    protocol         = "tcp"
-      cidr_blocks      = ["${var.IP_DEV}/16"]
+    from_port   = var.HTTPS_DEV_port
+    to_port     = var.HTTPS_DEV_port
+    protocol    = "tcp"
+    cidr_blocks = ["${var.IP_DEV}/16"]
   }
 
-# Below ingress allows APIs access from DEV VPC
+  # Below ingress allows APIs access from DEV VPC
 
   ingress {
-    from_port        = var.API_DEV_port
-      to_port          = var.API_DEV_port
-    protocol         = "tcp"
-       cidr_blocks      = ["${var.IP_DEV}/16"]
+    from_port   = var.API_DEV_port
+    to_port     = var.API_DEV_port
+    protocol    = "tcp"
+    cidr_blocks = ["${var.IP_DEV}/16"]
   }
 
-# Below ingress allows APIs access from Prod App Public IP.
+  # Below ingress allows APIs access from Prod App Public IP.
 
   ingress {
-    from_port        = var.API_PROD_port
-      to_port          = var.API_PROD_port
-    protocol         = "tcp"
-       cidr_blocks      = ["${aws_eip.example.public_ip}/32"]
+    from_port   = var.API_PROD_port
+    to_port     = var.API_PROD_port
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_eip.example.public_ip}/32"]
   }
 
- egress {
-    from_port        = var.splunk
-    to_port          = var.splunk
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  egress {
+    from_port   = var.splunk
+    to_port     = var.splunk
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 
 resource "aws_eip" "example" {
-   domain = "vpc"
+  domain = "vpc"
 }
