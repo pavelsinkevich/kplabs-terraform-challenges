9811396f16c21419ba4d0dafcb6e114e58359bc8
pavel_sinkevich1 <pavel.sinkevich@gmail.com>
2023-10-21 14:16:16 +0300

    Challenge 2 requirement 1

--- challenge-2/tf-challenge-2.tf
+++ challenge-2/tf-challenge-2.tf
@@ -11,9 +11,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "splunk" {
-  default = "8088"
-}
+
 resource "aws_security_group" "security_group_payment_app" {
   name        = "payment_app"
     description = "Application Security Group"
@@ -21,37 +19,37 @@ resource "aws_security_group" "security_group_payment_app" {
 
 # Below ingress allows HTTPS  from DEV VPC
   ingress {
-       from_port        = 443
-     to_port          = 443
+       from_port        = var.HTTPS_DEV_port
+     to_port          = var.HTTPS_DEV_port
     protocol         = "tcp"
-      cidr_blocks      = ["172.31.0.0/16"]
+      cidr_blocks      = ["${var.IP_DEV}/16"]
   }
 
 # Below ingress allows APIs access from DEV VPC
 
   ingress {
-    from_port        = 8080
-      to_port          = 8080
+    from_port        = var.API_DEV_port
+      to_port          = var.API_DEV_port
     protocol         = "tcp"
-       cidr_blocks      = ["172.31.0.0/16"]
+       cidr_blocks      = ["${var.IP_DEV}/16"]
   }
 
 # Below ingress allows APIs access from Prod App Public IP.
 
   ingress {
-    from_port        = 8443
-      to_port          = 8443
+    from_port        = var.API_PROD_port
+      to_port          = var.API_PROD_port
     protocol         = "tcp"
        cidr_blocks      = ["${aws_eip.example.public_ip}/32"]
   }
-}
+
  egress {
     from_port        = var.splunk
     to_port          = var.splunk
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
+}
 
 
 resource "aws_eip" "example" {
---
+++ challenge-2/variables.tf
@@ -0,0 +1,19 @@
+variable "splunk" {
+  default = "8088"
+}
+
+variable "HTTPS_DEV_port" {
+    default = "443"
+}
+
+variable "API_DEV_port" {
+    default = "8080"
+}
+
+variable "API_PROD_port" {
+    default = "8443"
+}
+
+variable "IP_DEV" {
+    default = "172.31.0.0"
+}
